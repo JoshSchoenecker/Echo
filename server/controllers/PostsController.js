87f04e5793b96d7254f9d54f018b93cae3ad0443
Josh Schoenecker <schoeneckerj@gmail.com>
2020-04-09 17:16:28 -0600

    Posts:getAll,getById,remove,create

---
+++ server/controllers/PostsController.js
@@ -0,0 +1,53 @@
+import express from "express";
+import BaseController from "../utils/BaseController";
+import { postsService } from "../services/PostsService";
+import { BadRequest } from "../utils/Errors";
+
+export class PostsController extends BaseController {
+  constructor() {
+    super("api/posts");
+    this.router
+      .get("", this.getAll)
+      .get("/:postId", this.getById)
+      .post("", this.create)
+      .delete("/:postId", this.remove)
+  }
+  async getAll(req, res, next) {
+    try {
+      let posts = await postsService.getAll()
+      res.send(posts)
+    } catch (error) {
+      next(error);
+    }
+  }
+  async create(req, res, next) {
+    try {
+      let post = await postsService.create(req.body);
+      res.send(post)
+    } catch (error) {
+      next(error);
+    }
+  }
+
+  async getById(req, res, next){
+    try {
+      let post = await postsService.getById(req.params.postId)
+      if (!post) {
+        throw new BadRequest("Invalid post Id")
+      }
+      res.send(post)
+    } catch (error){
+      next(error)
+    }
+  }
+
+  async remove(req, res, next) {
+    try {
+      let post = await postsService.remove(req.params.postId)
+      res.send('Deleted: ', post)
+    } catch (error){
+      next(error)
+    }
+  }
+}
+
--- server/models/Post.js
+++ server/models/Post.js
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const Value = new Schema(
+const Post = new Schema(
   {
+    author: {type: String, required: true},
     title: { type: String, required: true },
     description: { type: String, required: true }
   },
   { timestamps: true, toJSON: { virtuals: true } }
 );
 
-export default Value;
+export default Post;
+
