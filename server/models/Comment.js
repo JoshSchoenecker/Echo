194dc85901a3b80d31ed689b99e153990da4e47e
Preston Hess <hesspreston@outlook.com>
2020-04-10 14:40:04 -0700

    Client User Init

--- server/models/Comment.js
+++ server/models/Comment.js
@@ -1,10 +1,12 @@
 import mongoose from "mongoose";
 const Schema = mongoose.Schema;
+const ObjectId = mongoose.Schema.Types.ObjectId
+
 
 const Comment = new Schema({
-    author: { type: String, required: true },
-    title: { type: String, required: true },
-    description: { type: String, required: true }
+    authorId: { type: ObjectId, required: true },
+    comment: { type: String, required: true },
+    
 }, { timestamps: true, toJSON: { virtuals: true } });
 
 export default Comment;
