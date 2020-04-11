3c532fb42c61c1a34e2e0f546e1aa095f22eeb2a
Preston Hess <hesspreston@outlook.com>
2020-04-11 03:24:51 -0700

    postService

--- client/app/Services/PostService.js
+++ client/app/Services/PostService.js
@@ -13,21 +13,8 @@ class PostService {
       .post("", newPostObject)
       .then((res) => {
         let newPost = new Post(res.data);
-<<<<<<< HEAD
         let posts = [newPost, ...store.State.posts]
         store.commit('posts', posts)
-=======
-        console.log("newPost", newPost);
-        
-        store.State.posts.push(newPost);
-
-        // store.commit('posts', posts)
-        console.log(store.State.posts);
-        // let cars = [newCar, ..._store.State.cars]
-        // _store.commit('cars', cars)
-        //NOTE we could just call get cars again and it would handle getting all the cars and saving the state and redrawing.
-        // this.getCars()
->>>>>>> 1b8034efb677f1bfdf96e946adac1b9da36661d4
       })
       .catch((err) => console.error(err));
   }
